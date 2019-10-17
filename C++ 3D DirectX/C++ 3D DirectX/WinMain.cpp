27d8d1f4927bd86e115c9882b6b7c4f21a236d29
NamiNaziri <naminaziri1@gmail.com>
2019-10-17 15:57:21 +0330

    window massages

--- C++ 3D DirectX/C++ 3D DirectX/WinMain.cpp
+++ C++ 3D DirectX/C++ 3D DirectX/WinMain.cpp
@@ -1,14 +1,46 @@
 #include <windows.h>
+#include <iostream>
+#include <sstream>
 
 LRESULT CALLBACK WndProC(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg)
 	{
-	case WM_CLOSE:
-		PostQuitMessage( 69 );
-		break;
-	default:
-		break;
+		case WM_CLOSE:
+			PostQuitMessage( 69 );
+			break;
+
+		case WM_KEYDOWN:
+			if (wParam == 'F') 
+			{
+				SetWindowText(hWnd, "Respect");
+			}
+			break;
+		case WM_KEYUP:
+			if (wParam == 'F')
+			{
+				SetWindowText(hWnd, "Not Respect");
+			}
+			break;
+		case WM_CHAR :
+			{
+				static std::string title;
+				title.push_back((char)wParam);
+				SetWindowText(hWnd, title.c_str());
+			}
+			break;
+		case WM_LBUTTONDOWN:
+			{
+				const POINTS point = MAKEPOINTS(lParam);
+				std::ostringstream oss;
+				oss << "(" << point.x << "." << point.y << ")";
+				SetWindowText(hWnd, oss.str().c_str());
+				
+			}
+			break;
+			
+		default:
+			break;
 	}
 	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
